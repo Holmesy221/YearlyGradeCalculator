bf16cb62984030ea36313612505dd80334c4ee89
Holmesy221 <holmesy221@gmail.com>
2018-02-20 12:02:04 +0000

    Added menu option for Year 2 + 3 and Year3

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -3,19 +3,34 @@ import java.util.Scanner;
 public class GradeCalculator {
 
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-    Scanner scanner = new Scanner(System.in);
-    int a;
-    int b;
-    int c;
-    int d;
-    int e;
-    int f;
-    double average = 0;
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+        double average = 0;
 
 
+        System.out.println("Welcome to the grade calculator.");
+        System.out.println("Option one: Year 2 + 3 final mark");
+        System.out.println("Option two: Year 3 final mark");
 
+        System.out.println("For option one please enter the number '1' ");
+        System.out.println("For option two please enter the number '2' ");
+        System.out.println();
+
+
+        switch (scanner.nextInt()) {
+
+            case 1:
+                System.out.println("You choose option one");
+                break;
+
+            case 2:
 
         System.out.println("Enter your first module mark: ");
         a = scanner.nextInt();
@@ -40,22 +55,13 @@ public class GradeCalculator {
         average = (double) sum / 6;
 
         System.out.println("Your yearly mark is: " + average);
+        break;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
     }
-}
+
 
 
